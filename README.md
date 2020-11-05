94a2b3ddee9720a3a12aa2685cd25f0de7157239
Ramón Torres <ramon@digitalinclusionfoundation.org>
2020-11-04 19:18:39 -0600

    chore: publish under LGPLv3

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # @opencerta/essentials
 
-OpenCerta Essentials
+OpenCerta Essentials library
+
+## License
+
+The @opencerta/essentials library is licensed under the
+[GNU Lesser GeneralPublic License v3.0](https://www.gnu.org/licenses/lgpl-3.0.en.html),
+also included in this repository in the `COPYING.LESSER` file.
--- src/lib/compression/CompressionStrategy.spec.ts
+++ src/lib/compression/CompressionStrategy.spec.ts
@@ -1,3 +1,20 @@
+// @opencerta/essentials
+// Copyright (C) 2020 OpenCerta
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 /* spell-checker: disable */
 import test from 'ava';
 
@@ -29,16 +46,15 @@ Object.keys(CompressionAlgorithm)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   .filter((key: any) => isNaN(Number(CompressionAlgorithm[key])))
   .forEach((algorithm) => {
-    test(`retrieve '${
-      CompressionAlgorithm[Number(algorithm)]
-    }' compression strategy`, async (t) => {
-      const strategy: CompressionStrategy = registry.getStrategy(
-        Number(algorithm)
-      );
-      const compressed = strategy.compress(text);
-      const decompressed = strategy.decompress(compressed);
-      t.deepEqual(decompressed, text);
-    });
+    test(`retrieve '${CompressionAlgorithm[Number(algorithm)]
+      }' compression strategy`, async (t) => {
+        const strategy: CompressionStrategy = registry.getStrategy(
+          Number(algorithm)
+        );
+        const compressed = strategy.compress(text);
+        const decompressed = strategy.decompress(compressed);
+        t.deepEqual(decompressed, text);
+      });
   });
 
 /**
